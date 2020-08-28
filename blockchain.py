2fa587b62dea577bcdb4e3b43671d1601f00b8b9
MackRoe <elaine.music@students.makeschool.com>
2020-08-27 20:41:51 -0500

    create Blockchain class

--- blockchain.py
+++ blockchain.py
@@ -1,40 +1,42 @@
-def __init__(self):
+class Blockchain(object):
 
-    self.chain = []
+    def __init__(self):
 
-    self.current_transactions = []
+        self.chain = []
 
+        self.current_transactions = []
 
-def new_block(self):
+    def new_block(self):
 
-    # This function creates new blocks and then adds to the existing chain
+        # This function creates new blocks and then adds to the existing chain
 
-    pass
+        pass
 
+    def new_transaction(self):
 
-def new_transaction(self):
+        # This function adds a new transaction to already existing transactions
 
-    # This function adds a new transaction to already existing transactions
+        pass
 
-    pass
+    @staticmethod
+    def hash(block):
+        # Used for hashing a blocks
+        pass
 
+    def register_node():
+        # To register a new node and add it to the network
+        pass
 
-@staticmethod
-def hash(block):
-    # Used for hashing a blocks
-    pass
+    def valid_proof():
+        # To ensure whether a submitted block to the blockchain solves the
+        # problem
+        pass
 
+    def valid_chain():
+        # Check if subsequent blocks in the chain are valid
+        pass
 
-def register_node():
-    # To register a new node and add it to the network
-    pass
-
-
-def valid_proof():
-    # To ensure whether a submitted block to the blockchain solves the problem
-    pass
-
-
-def valid_chain():
-    # Check if subsequent blocks in the chain are valid
-    pass
+    @property
+    def last_block(self):
+        # calls and returns the last block of the chain
+        pass
