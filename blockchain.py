69bc61008e550ae824116d035fc44df68806fc49
MackRoe <elaine.music@students.makeschool.com>
2020-08-27 20:50:11 -0500

    build new_transaction function

--- blockchain.py
+++ blockchain.py
@@ -13,26 +13,29 @@ class Blockchain(object):
         pass
 
     def new_transaction(self):
-
+        ''' Create a new transaction that will be sent to the next block.
+        Contains three variables: sender, recipient, amount '''
         # This function adds a new transaction to already existing transactions
-
-        pass
+        self.current_transactions.append(
+            {‘sender’: sender, ‘recipient’: recipient, ‘amount’: amount}
+                )
+        return self.last_block[‘index’] + 1
 
     @staticmethod
     def hash(block):
         # Used for hashing a blocks
         pass
 
-    def register_node():
+    def register_node(self):
         # To register a new node and add it to the network
         pass
 
-    def valid_proof():
+    def valid_proof(self):
         # To ensure whether a submitted block to the blockchain solves the
         # problem
         pass
 
-    def valid_chain():
+    def valid_chain(self):
         # Check if subsequent blocks in the chain are valid
         pass
 
