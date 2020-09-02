ce276ef190c26cf93b2e03a758438a21501de069
MackRoe <elaine.music@students.makeschool.com>
2020-09-01 21:47:02 -0500

    add routes outline

--- app.py
+++ app.py
@@ -14,3 +14,14 @@ def mine():
 @app_route('/transactions/new', methods=['POST'])
 def new_transaction():
     return 'Adding a new Transaction'
+
+@app_router('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
